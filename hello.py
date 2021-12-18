06e50289d60754e276e5c8aff48d3747368ad867
HIMANSHU SAHOO <himansu12145@gmail.com>
2021-12-18 07:33:04 +0530

    not the least of python as well

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("hello_world")
+h="hello here i am here to tach you all the relativity theory of python on other languages"
+print(h)
+a=20
+b=30
+c=3
+if a>b:
+    print(a)
+if b>c:
+    print(b)
+if c>a:
+    print(c)
